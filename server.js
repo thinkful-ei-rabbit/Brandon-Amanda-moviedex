af4f0587f99daf3519a4d773b10c7937451ecd6f
Brandon Weiss <brandonweiss2008@gmail.com>
2020-09-15 16:03:43 -0400

    added new features

--- server.js
+++ server.js
@@ -1,16 +1,58 @@
 const express = require('express')
 const morgan = require('morgan')
+const movieData = require('./movie-data.js')
 
 const app = express()
 
 app.use(morgan('dev'))
 
-app.use((req, res) => {
-  res.send('Hello, world!')
+// app.use((req, res) => {
+//   res.send('Hello, world!')
+// })
+
+
+app.get('/movies', (req,res) => {
+  let data = [ ...movieData ];
+  const { genre, country, avg_vote } = req.query
+
+if(genre) {
+  if(!['Animation', 'Drama', 'Romantic', 'Comedy', 'Spy', 'Crime', 'Thriller', 'Adventure', 'Documentary', 'Horror', 'Action', 'Western', 'History', 'Biography', 'Musical', 'Fantasy', 'War', 'Grotesque'].includes(genre)) {
+    return res.status(400).send('Please senter an appropriate genre');
+  }
+}
+
+if(country) {
+  if(!['United States', 'Italy', 'Germany', 'Israel', 'Great Britain', 'France', 'Hungary', 'China', 'Canada', 'Spain', 'Japan'].includes(country)) {
+    return res.status(400).send('Please enter an appropriate country');
+  }
+}
+
+if(avg_vote) {
+  if(avg_vote > 10 || avg_vote < 0) {
+    return res.status(400).send('Avg vote must be between 0 and 10');
+  }
+}
+
+if(genre) {
+  data = data.filter(movie => {
+    return movie.genre.includes(genre)
+  })
+}
+
+if(country) {
+  data = data.filter(movie => {
+    return movie.country.includes(country)
+  })
+}
+
+if(avg_vote) {
+  data = data.filter(movie => {
+    return movie.avg_vote >= avg_vote
+  })
+}
+return res.json(data);
 })
 
-const PORT = 8000
-
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
+app.listen(8000, () => {
+  console.log(`Server listening at http://localhost:8000`)
 })
