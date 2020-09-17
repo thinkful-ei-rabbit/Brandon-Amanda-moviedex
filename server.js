bf73946a877d901f74a49565f0006da063a8eca2
Amanda McCabe <amccabe1316@gmail.com>
2020-09-17 12:06:01 -0600

    Update server.js

--- server.js
+++ server.js
@@ -11,9 +11,6 @@ const app = express();
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
-// app.use((req, res) => {
-//   res.send('Hello, world!')
-// })
 
 app.use(function validateBearerToken(req, res, next){
   const apiToken = process.env.API_TOKEN;
@@ -27,27 +24,9 @@ app.use(function validateBearerToken(req, res, next){
   next();
 });
 
-app.get('/movies', (req,res) => {
+app.get('/movie', (req,res) => {
   let data = [ ...movieData ];
   const { genre, country, avg_vote } = req.query;
-
-  if(genre) {
-    if(!['animation', 'drama', 'romantic', 'comedy', 'spy', 'crime', 'thriller', 'adventure', 'documentary', 'horror', 'action', 'western', 'history', 'biography', 'musical', 'fantasy', 'war', 'grotesque'].includes(genre.toLowerCase())) {
-      return res.status(400).send('Please senter an appropriate genre');
-    }
-  }
-
-  if(country) {
-    if(!['united states', 'italy', 'germany', 'israel', 'great britain', 'france', 'hungary', 'china', 'canada', 'spain', 'japan'].includes(country.toLowerCase())) {
-      return res.status(400).send('Please enter an appropriate country');
-    }
-  }
-
-  if(avg_vote) {
-    if(avg_vote > 10 || avg_vote < 0) {
-      return res.status(400).send('Avg vote must be between 0 and 10');
-    }
-  }
 
   if(genre) {
     data = data.filter(movie => {
